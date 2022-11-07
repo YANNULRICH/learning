a83b057ce38ab74ead4a7fa814acaa07ec1107a5
YANNULRICH <106261348+YANNULRICH@users.noreply.github.com>
2022-11-07 09:54:24 -0500

    Add files via upload

---
+++ d.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int number1,number2;
+	printf("enter the first number:");
+	scanf("%d",&number1);
+	printf("enter the second number:");
+	scanf("%d",&number2);
+	printf("%d + %d = %d ",number1,number2,number1+number2);
+}
